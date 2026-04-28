b5810c1cf450ac00d90ea5d90d43ba5ad1ac45d5
dzianispiatrou <piatroudzianis@gmail.com>
2026-04-28 22:27:35 +0300

    Auto-update mats.lua: 4/28/2026, 3:27:35 PM

--- mats.lua
+++ mats.lua
@@ -117,6 +117,7 @@ MAT = {
     [45994] = 110000000,  --lost ring
     [45995] = 110000000,  --forgotten necklace
     [54069] = 550000000,  --Blazing Hippogryph
+    [49286] = 311666666.6666666,  --X-51 Nether-Rocket X-TREME
     [49282] = 275000000,  --Big Battle Bear
     [49285] = 128333333.33333333,  --X-51 Nether-Rocket
     [21168] = 275000000,  --Baby Shark
