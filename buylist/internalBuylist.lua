f0f25c838e08e65b2b4644c2aa4de9a3548507c9
dzianispiatrou <piatroudzianis@gmail.com>
2026-09-21 15:30:28 +0300

    Auto-update buylist/internalBuylist.lua: 9/21/2026, 8:30:28 AM

--- buylist/internalBuylist.lua
+++ buylist/internalBuylist.lua
@@ -70,4 +70,5 @@ INTERNAL_BUYLIST = {
     [46379] = 1000000,  --Flask of Stoneblood
     [40199] = 25000,  --Pygmy Suckerfish
     [40769] = 125774.99999999999,  -- Scrapbot Construction Kit
+    [33448] = 190000,  --
 }
